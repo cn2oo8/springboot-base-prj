49d5e90ba94fb11b21032e88358acd6a6a8aaff9
david <zhangshibin@jd.com>
2020-07-30 11:13:59 +0800

    修改输出路径

--- ui-starter/vue.config.js
+++ ui-starter/vue.config.js
@@ -29,13 +29,14 @@ module.exports = {
   // see https://github.com/vuejs/vue-cli/blob/dev/docs/webpack.md
   // 如果你不需要使用eslint，把lintOnSave设为false即可
   lintOnSave: true,
+  outputDir: '../baseproject-springboot-web/src/main/resources/static',
   chainWebpack: config => {
     config.resolve.alias
       .set('@', resolve('src')) // key,value自行定义，比如.set('@@', resolve('src/components'))
       .set('_c', resolve('src/components'))
   },
   // 设为false打包时不生成.map文件
-  productionSourceMap: false,
+  productionSourceMap: true,
   // 这里写你调用接口的基础路径，来解决跨域，如果设置了代理，那你本地开发环境的axios的baseUrl要写为 '' ，即空字符串
   // devServer: {
   //   proxy: 'localhost:3000'
